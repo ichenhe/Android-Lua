3abb9921c3deebcf6b201990345bba7bac246421
Chenhe <liangchenhe55@gmail.com>
2021-12-29 21:57:26 +0800

    upgrade AGP and dependency

--- app/src/main/java/cc/chenhe/lib/androidlua/demo/MainActivity.java
+++ app/src/main/java/cc/chenhe/lib/androidlua/demo/MainActivity.java
@@ -4,10 +4,9 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.keplerproject.luajava.LuaState;
 import org.keplerproject.luajava.LuaStateFactory;
@@ -16,7 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 @SuppressLint("SetTextI18n")
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private LuaState lua = null;
     private TextView textView;
@@ -35,110 +34,87 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lua.openLibs();
         lua.LdoString(readAssetsTxt(this, "test.lua"));
 
-        findViewById(R.id.btnGetV1).setOnClickListener(this);
-        findViewById(R.id.btnSetV2).setOnClickListener(this);
-        findViewById(R.id.btnGetV2).setOnClickListener(this);
-        findViewById(R.id.btnInjectJavaFun).setOnClickListener(this);
-        findViewById(R.id.btnCallInjectedFun).setOnClickListener(this);
-        findViewById(R.id.btnGetLuaTable).setOnClickListener(this);
-        findViewById(R.id.btnSetLuaTable).setOnClickListener(this);
-        findViewById(R.id.btnCallLua).setOnClickListener(this);
-        findViewById(R.id.btnInjectJavaObj).setOnClickListener(this);
-        findViewById(R.id.btnLuaCallback).setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View view) {
         StringBuilder s = new StringBuilder();
-        switch (view.getId()) {
-            case R.id.btnGetV1:
-                lua.getGlobal("v1");
-                textView.setText(lua.toString(-1));
-                lua.pop(1);
-                break;
-            case R.id.btnSetV2:
-                lua.pushString("value from java");
-                lua.setGlobal("v2");
-                break;
-            case R.id.btnGetV2:
-                lua.getGlobal("v2");
-                textView.setText(lua.toString(-1));
-                lua.pop(1);
-                break;
-            case R.id.btnInjectJavaFun:
-                new MyJavaFunction(lua).register();
-                break;
-            case R.id.btnCallInjectedFun:
-                lua.LdoString("return getTime(' - passing by lua')");
-                textView.setText(lua.toString(-1));
-                lua.pop(1);
-                break;
-            case R.id.btnGetLuaTable:
-                lua.getGlobal("table");
-                s.delete(0, s.length());
-                if (lua.isTable(-1)) {
-                    lua.pushNil();
-                    while (lua.next(-2) != 0) {
-                        s.append(lua.toString(-2)).append(" = ")
-                                .append(lua.toString(-1)).append("\n");
-                        lua.pop(1);
-                    }
+        findViewById(R.id.btnGetV1).setOnClickListener(view -> {
+            lua.getGlobal("v1");
+            textView.setText(lua.toString(-1));
+            lua.pop(1);
+        });
+        findViewById(R.id.btnSetV2).setOnClickListener(view -> {
+            lua.pushString("value from java");
+            lua.setGlobal("v2");
+        });
+        findViewById(R.id.btnGetV2).setOnClickListener(view -> {
+            lua.getGlobal("v2");
+            textView.setText(lua.toString(-1));
+            lua.pop(1);
+        });
+        findViewById(R.id.btnInjectJavaFun).setOnClickListener(view -> new MyJavaFunction(lua).register());
+        findViewById(R.id.btnCallInjectedFun).setOnClickListener(view -> {
+            lua.LdoString("return getTime(' - passing by lua')");
+            textView.setText(lua.toString(-1));
+            lua.pop(1);
+        });
+        findViewById(R.id.btnGetLuaTable).setOnClickListener(view -> {
+            lua.getGlobal("table");
+            s.delete(0, s.length());
+            if (lua.isTable(-1)) {
+                lua.pushNil();
+                while (lua.next(-2) != 0) {
+                    s.append(lua.toString(-2)).append(" = ")
+                            .append(lua.toString(-1)).append("\n");
                     lua.pop(1);
-                    textView.setText(s.toString());
                 }
-                break;
-            case R.id.btnSetLuaTable:
-                lua.newTable();
-                lua.pushString("from");
-                lua.pushString("java");
-                lua.setTable(-3);
-                lua.pushString("value");
-                lua.pushString("Hello lua");
-                lua.setTable(-3);
-                lua.setGlobal("table");
-                break;
-            case R.id.btnCallLua:
-                s.delete(0, s.length());
-                lua.getGlobal("extreme");
-                lua.pushNumber(15.6);
-                lua.pushNumber(0.8);
-                lua.pushNumber(189);
-                lua.pcall(3, 2, 0);
-                s.append("max:").append(lua.toString(-2)).append(" min:").append(lua.toString(-1));
+                lua.pop(1);
                 textView.setText(s.toString());
-                lua.pop(2);
-                break;
-            case R.id.btnInjectJavaObj:
-                lua.pushJavaObject(textView);
-                lua.setGlobal("textView");
-                lua.pushInteger(Color.RED);
-                lua.setGlobal("red");
-                lua.LdoString("textView:setTextColor(red)");
-                break;
-            case R.id.btnLuaCallback:
-                textView.setText("Loading...");
-                new AsyncJavaFunction(lua).register();
-                lua.getGlobal("luaCallback");
-                lua.pushJavaObject(textView);
-                lua.pcall(1, 0, 0);
-                break;
-        }
-        Log.i("LuaStack", lua.dumpStack());
+            }
+        });
+        findViewById(R.id.btnSetLuaTable).setOnClickListener(view -> {
+            lua.newTable();
+            lua.pushString("from");
+            lua.pushString("java");
+            lua.setTable(-3);
+            lua.pushString("value");
+            lua.pushString("Hello lua");
+            lua.setTable(-3);
+            lua.setGlobal("table");
+        });
+        findViewById(R.id.btnCallLua).setOnClickListener(view -> {
+            s.delete(0, s.length());
+            lua.getGlobal("extreme");
+            lua.pushNumber(15.6);
+            lua.pushNumber(0.8);
+            lua.pushNumber(189);
+            lua.pcall(3, 2, 0);
+            s.append("max:").append(lua.toString(-2)).append(" min:").append(lua.toString(-1));
+            textView.setText(s.toString());
+            lua.pop(2);
+        });
+        findViewById(R.id.btnInjectJavaObj).setOnClickListener(view -> {
+            lua.pushJavaObject(textView);
+            lua.setGlobal("textView");
+            lua.pushInteger(Color.RED);
+            lua.setGlobal("red");
+            lua.LdoString("textView:setTextColor(red)");
+        });
+        findViewById(R.id.btnLuaCallback).setOnClickListener(view -> {
+            textView.setText("Loading...");
+            new AsyncJavaFunction(lua).register();
+            lua.getGlobal("luaCallback");
+            lua.pushJavaObject(textView);
+            lua.pcall(1, 0, 0);
+        });
+
     }
 
     public static String readAssetsTxt(Context context, String fileName) {
         try {
             InputStream is = context.getAssets().open(fileName);
             int size = is.available();
-            // Read the entire asset into a local byte buffer.
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            // Convert the buffer into a string.
-            String text = new String(buffer, "utf-8");
-            // Finally stick the string into the text view.
-            return text;
+            return new String(buffer, "utf-8");
         } catch (IOException e) {
             e.printStackTrace();
         }
